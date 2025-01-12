a0999d9c91f463912f3fbbad45d93d049637d7e8
Gwihwan Go <iejw1914@gmail.com>
2025-01-13 05:40:36 +0800

    python coverage collect script written

---
+++ python_coverage.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -x
+
+# Check if the required parameters are provided
+if [ -z "$1" ]; then
+    echo "Error: Target project path is missing."
+    echo "Usage: $0 <target_project_path> <test_save_dir> [report_dir]"
+    exit 1
+fi
+
+if [ -z "$2" ]; then
+    echo "Error: Test file save path is missing."
+    echo "Usage: $0 <target_project_path> <test_save_dir> [report_dir]"
+    exit 1
+fi
+
+
+# Input parameters
+TARGET_PROJECT_PATH=$1
+TEST_DIR=$2
+REPORT_DIR=${3:-"${TEST_DIR}-report"}  # Default value if not provided
+mkdir -p "$REPORT_DIR"
+# Navigate to target project path
+cd "$TARGET_PROJECT_PATH" || exit 1
+PYTHONPATH="$TARGET_PROJECT_PATH/src":"$TARGET_PROJECT_PATH/src/black" ## hardcoded,,,,
+python3 -m coverage run --data-file="$REPORT_DIR/.coverage" -m pytest --continue-on-collection-errors $TEST_DIR
+python3 -m coverage report --data-file="$REPORT_DIR/.coverage"
