21c50b581f9f85b90fddcb43d47a74e7ff76be6c
Gwihwan-Go <iejw1914@gmail.com>
2025-03-14 04:37:14 +0000

    Testing environment setup(vscode-test, dependency extensions)

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,24 +1,40 @@
+import * as cp from 'child_process';
 import * as path from 'path';
-
-import { runTests } from '@vscode/test-electron';
+import {
+  downloadAndUnzipVSCode,
+  resolveCliArgsFromVSCodeExecutablePath,
+  runTests
+} from '@vscode/test-electron';
 
 async function main() {
-	try {
-		// The folder containing the Extension Manifest package.json
-		// Passed to `--extensionDevelopmentPath`
-		const extensionDevelopmentPath = path.resolve(__dirname, '../../');
+  try {
+    const extensionDevelopmentPath = path.resolve(__dirname, '../../../');
+    const extensionTestsPath = path.resolve(__dirname, './suite/index');
+    const vscodeExecutablePath = await downloadAndUnzipVSCode('1.98.2');
+    const [cliPath, ...args] = resolveCliArgsFromVSCodeExecutablePath(vscodeExecutablePath);
 
-		// The path to the extension test script
-		// Passed to --extensionTestsPath
-		const extensionTestsPath = path.resolve(__dirname, './suite/index');
+    // Use cp.spawn / cp.exec for custom setup
+	// const installExtensions = ['ms-python.python', 'oracle.oracle-java', 'golang.go'];
+    cp.spawnSync(
+      cliPath,
+		[...args, '--install-extension', 'ms-python.python', '--install-extension', 'redhat.java', '--install-extension', 'golang.go'],
+		{
+        encoding: 'utf-8',
+        stdio: 'inherit'
+      }
+    );
 
-		console.log("testing")
-		// Download VS Code, unzip it and run the integration test
-		await runTests({ extensionDevelopmentPath, extensionTestsPath });
-	} catch {
-		console.error('Failed to run tests');
-		process.exit(1);
-	}
+    // Run the extension test
+    await runTests({
+      // Use the specified `code` executable
+      vscodeExecutablePath,
+      extensionDevelopmentPath,
+      extensionTestsPath
+    });
+  } catch (err) {
+    console.error('Failed to run tests');
+    process.exit(1);
+  }
 }
 
 main();
